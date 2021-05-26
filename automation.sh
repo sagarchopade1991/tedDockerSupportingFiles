f787579c46b1998bb746012349de6b6c069fdbfd
Shreyas Chaudhari <shreya.chaudhari@sbdinc.com>
2021-05-26 16:35:21 +0530

    Changed parameters in bash script

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-accept input
+# accept input
 echo "Enter Resource Group Name:"
 read resource_group
 echo "Enter Storage Account Name:"
@@ -22,6 +22,9 @@ apt-get install apt-transport-https \
                 curl gnupg \
                 lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
@@ -36,13 +39,14 @@ echo "Please copy the link and paste it in the browser"
 echo "***************************************************"
 echo "***************************************************"
 az login
-
+# add extention to az
+az extension add --name azure-iot
 
 # container registry login 
 az acr login --name iotsharedcontainerregistry
 
 # create iot device 
-az iot hub device-identity create -n $resource_group -d $iot_device_name --ee false
+az iot hub device-identity create -n $iot_hub -d $iot_device_name --ee false
 
 # create .env and copy content from env.temp
 true > .env && cp env.temp .env
@@ -51,8 +55,9 @@ true > .env && cp env.temp .env
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs build-essential
 
-# # install npm packages and run nodejs app to set environment variables
+# install npm packages and run nodejs app to set environment variables
+
 npm install && node set_env.js $resource_group $storage_acc $iot_hub $iot_device_name $cosmos_acc $vm_name
-# npm install && node set_env.js SharedResources-IoT-Projects strgpocenv iot-hub-poc-env ted-device ted-cosmos-db TedVMTest
 
 docker-compose up -d
+
