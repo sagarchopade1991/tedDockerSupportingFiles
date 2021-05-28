b5bac0faf84ede8a531df9d5a682360ea4510e96
sagarchopade1991 <84382852+sagarchopade1991@users.noreply.github.com>
2021-05-28 17:05:58 +0530

    update file

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-git clone https://github.com/sagarchopade1991/tedDockerSupportingFiles.git
-cd tedDockerSupportingFiles
-sudo chmod +x automation.sh
-sudo bash automation.sh
+touch abc.txt
